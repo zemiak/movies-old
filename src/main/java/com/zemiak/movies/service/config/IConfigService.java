82d3bd0fabbeb431ca6540099ae3fb5c2a7b3b65
zemiak <zemiak@gmail.com>
2014-04-16 20:07:53 +0200

    Config works for GF and WFLY/2

---
+++ src/main/java/com/zemiak/movies/service/config/IConfigService.java
@@ -0,0 +1,13 @@
+package com.zemiak.movies.service.config;
+
+public interface IConfigService {
+    String getBackendUrl();
+    String getFfmpeg();
+    String getImgPath();
+    String getMailFrom();
+    String getMailSubject();
+    String getMailTo();
+    String getMp4info();
+    String getMp4tags();
+    String getPath();
+}
