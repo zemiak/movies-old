b0d9684ef5e9088398f01dcfac0d0677e04dde64
zemiak <zemiak@gmail.com>
2014-04-18 21:32:28 +0200

    ConfigBean package has been changed

--- src/main/java/com/zemiak/movies/service/configuration/ConfigBean.java
+++ src/main/java/com/zemiak/movies/service/configuration/ConfigBean.java
@@ -1,9 +1,10 @@
 package com.zemiak.movies.service.configuration;
 
 import com.zemiak.movies.lookup.CDILookup;
+import java.io.Serializable;
 
-public class ConfigBean {
-    private Configuration conf;
+public class ConfigBean implements Serializable {
+    private final Configuration conf;
 
     public ConfigBean() {
         conf = new CDILookup().lookup(Configuration.class);
