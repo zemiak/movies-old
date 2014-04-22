308c81436ad75cb30133efc48f9b04b268a3f387
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2014-04-22 14:58:09 +0200

    Preparing for fullscreen video #2

--- src/main/webapp/play/js/fullscreen.js
+++ src/main/webapp/play/js/fullscreen.js
@@ -1,3 +1,15 @@
-function fullScreenVideo(id) {
-    console.log("Video", id);
+function fullScreenVideoInit(id) {
+    var element = document.getElementById(id);
+    element.addEventListener("play", fullScreenVideoTrigger, false);
+}
+
+function fullScreenVideoTrigger(event) {
+    var element = event.target;
+    var classToAdd = 'fullscreen-video';
+
+    if ((' '+element.className+' ').indexOf(' '+classToAdd+' ') == -1) {
+        element.className = element.className === '' ? classToAdd : element.className + ' ' + classToAdd;
+    }
+
+    document.documentElement.webkitRequestFullscreen();
 }
