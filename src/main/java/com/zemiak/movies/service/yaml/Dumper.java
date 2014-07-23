420dcbb20a78d103d2a5b024c16d16eb591d35be
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2014-07-23 08:18:32 +0200

    Data dump preparations

--- src/main/java/com/zemiak/movies/service/yaml/Dumper.java
+++ src/main/java/com/zemiak/movies/service/yaml/Dumper.java
@@ -4,18 +4,31 @@ import com.zemiak.movies.domain.Genre;
 import com.zemiak.movies.domain.Language;
 import com.zemiak.movies.domain.Movie;
 import com.zemiak.movies.domain.Serie;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.Startup;
+import javax.inject.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
+@Startup
+@Singleton
 public class Dumper {
+    private static final Logger LOG = Logger.getLogger(Dumper.class.getName());
+
     @PersistenceContext
     EntityManager em;
 
-    public String dump() {
+    @PostConstruct
+    public void dump() {
         List<Object> entities = new ArrayList<>();
 
         loadGenres(entities);
@@ -26,25 +39,32 @@ public class Dumper {
         DumperOptions options = new DumperOptions();
         options.setPrettyFlow(true);
 
-        Yaml yaml = new Yaml(options);
-        return yaml.dump(entities);
+        final Yaml yaml = new Yaml(options);
+        final String dumped = yaml.dump(entities);
+
+        try (OutputStream os = new FileOutputStream("/dev-data.yml")) {
+            os.write(dumped.getBytes());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "File save problem", ex);
+        }
     }
 
+
     private void loadGenres(List<Object> entities) {
-        em.createNamedQuery("Genre.findAll", Genre.class).getResultList().stream().forEach((_item) -> {
-            entities.add(_item);
+        em.createNamedQuery("Genre.findAll", Genre.class).getResultList().stream().forEach((item) -> {
+            entities.add(item);
         });
     }
 
     private void loadSeries(List<Object> entities) {
-        em.createNamedQuery("Serie.findAll", Serie.class).getResultList().stream().forEach((_item) -> {
-            entities.add(_item);
+        em.createNamedQuery("Serie.findAll", Serie.class).getResultList().stream().forEach((item) -> {
+            entities.add(item);
         });
     }
 
     private void loadLanguages(List<Object> entities) {
-        em.createNamedQuery("Language.findAll", Language.class).getResultList().stream().forEach((_item) -> {
-            entities.add(_item);
+        em.createNamedQuery("Language.findAll", Language.class).getResultList().stream().forEach((item) -> {
+            entities.add(item);
         });
     }
 
