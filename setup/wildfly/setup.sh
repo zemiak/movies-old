fa3491a036282feb5d0b83574edea71e87a5b183
zemiak <zemiak@gmail.com>
2014-10-27 20:06:15 +0100

    Wildfly setup fixes

--- setup/wildfly/setup.sh
+++ setup/wildfly/setup.sh
@@ -4,18 +4,24 @@ MAC=/Users/vasko/bin/wildfly8/
 LINUX=/home/wildfly/wildfly8/
 WINDOWS=/e/Java/wildfly8/
 
-if [ -d "$MAC" ]
+if [ -d "$JBOSS_HOME" ]
+then
+    echo Found >/dev/null
+elif [ -d "$MAC" ]
 then
     export JBOSS_HOME="$MAC"
 elif [ -d "$LINUX" ]
 then
     export JBOSS_HOME="$LINUX"
 elif [ -d "$WINDOWS" ]
+then
     export JBOSS_HOME="$WINDOWS"
+else
+    echo "Cannot find Wildfly Home"
+    exit 3
 fi
 
-
-CLI="${JBOSS_HOME%%/}bin/jboss-cli.sh"
+CLI="${JBOSS_HOME%%/}/bin/jboss-cli.sh"
 
 if [ ! -x "$CLI" ]
 then
