275b911a24b18cc1c09429d55b42fdae8025cd6e
Miroslav Vasko <vasko@docker-tryout.cp.local>
2014-10-30 05:13:07 -0400

    Changed conf for dockerized running environment

--- src/main/resources/php/conf.php
+++ src/main/resources/php/conf.php
@@ -2,9 +2,7 @@
 
 return array(
        "name" => "movies",
-       "host" => "lenovo-server.local",
-       "hostname" => "lenovo-server.local",
+       "hostname" => "movies-db",
        "port" => 5432,
-       "user" => "movies_user",
        "username" => "movies_user",
        "password" => "movies/0");
