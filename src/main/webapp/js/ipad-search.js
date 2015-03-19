00d11742e48f51af53c86776070637a4eb58c5a0
zemiak <zemiak@gmail.com>
2015-03-19 20:16:23 +0100

    iPhone JSF/iUI UI implemented

--- src/main/webapp/js/ipad-search.js
+++ src/main/webapp/js/ipad-search.js
@@ -1,3 +1,11 @@
 function searchClick() {
-    
+    window.location = "searchResults.xhtml?query=" + encodeURIComponent($("#query").value());
+}
+
+function searchKeyUp(e) {
+    console.log("keyUp", e);
+    var charCode = (typeof e.which === "number") ? e.which : e.keyCode;
+    if (charCode === 13) {
+        searchClick();
+    }
 }
