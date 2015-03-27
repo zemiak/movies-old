d49937ec2b65a80d208d5052632134139521e3f2
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-03-27 08:31:59 +0100

    Removed php player, glassfish support

--- src/dev/resources/itunes/sync-itunes.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-test -f /Volumes/media/Movies/itunes-refresh || exit 0
-osascript /Users/vasko/Documents/itunes-replace-movies.scpt
-rm -f /Volumes/media/Movies/itunes-refresh
--- src/prod/resources/php/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-       "name" => "movies",
-       "hostname" => "127.0.0.1",
-       "port" => 5432,
-       "username" => "movies_user",
-       "password" => "movies/0");
