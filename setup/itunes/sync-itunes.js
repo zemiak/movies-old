76d2312906249c84bc304c07cf47f2a4ebee4e26
zemiak <zemiak@gmail.com>
2015-05-16 09:38:54 +0200

    Marker file cannot be deleted on a RO file system

--- setup/itunes/sync-itunes.js
+++ setup/itunes/sync-itunes.js
@@ -8,17 +8,10 @@ var shell = WScript.CreateObject("WScript.Shell");
 var itunes = WScript.CreateObject("iTunes.Application");
 var playlist = itunes.LibraryPlaylist;
 
-WScript.Echo("Removing " + playlist.Tracks.Count + " tracks...");
 while (playlist.Tracks.Count > 0) {
 	var track = playlist.Tracks.Item(1);
 	track.Delete();
 }
 
-WScript.Echo("Adding Music ...");
 playlist.AddFile("M:\\Music");
-
-WScript.Echo("Adding Movies ...");
 playlist.AddFile("M:\\Movies");
-
-fso.DeleteFile(MARKER_FILE);
-WScript.Echo("Done.");
