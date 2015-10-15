c998eaf8766a143641c73339ed2856247862ba92
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-10-15 11:21:11 +0200

    Moving to the H2 database #2

--- src/main/setup/install-fresh-wildfly.sh
+++ src/main/setup/install-fresh-wildfly.sh
@@ -7,31 +7,23 @@ INSTALL=~/bin
 TARGET=wildfly10
 
 cd $INSTALL
-killall java
 test -d $TARGET && rm -rf $TARGET
 cp $DOWNLOAD ./
 unzip $WILDFLY.zip >/dev/null
 rm $WILDFLY.zip
 mv $WILDFLY $TARGET
 cd $TARGET/standalone/configuration
-rm standalone-*.xml
+rm standalone*.xml
+cp $PROJECT/src/main/setup/standalone-dev.xml ./standalone.xml
 ln -s standalone.xml standalone-full.xml
 ln -s standalone.xml standalone-full-ha.xml
 ln -s standalone.xml standalone-ha.xml
 cd ../../..
 
 # Restore the database
+rm -rf /tmp/movies.*
 java \
     -cp ./$TARGET/modules/system/layers/base/com/h2database/h2/main/h2-1.3.173.jar \
     org.h2.tools.RunScript -url jdbc:h2:/tmp/movies -user sa -script $PROJECT/src/dev/resources/dump.sql
 
 ./$TARGET/bin/add-user.sh vasko 123456 --silent
-./$TARGET/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
-sleep 5s
-
-cd $PROJECT/src/main/setup
-sh setup.sh
-
-sleep 3s
-killall java
-echo "Finished!"
